df019114b1bc0d1488077bb837ac3f4160d11155
Yves Molina <yvestmolina@gmail.com>
2021-05-25 01:00:19 +0200

    change metadata

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,18 +4,30 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import "../style/main.scss"
 import { Helmet } from "react-helmet"
 
-export default function Home() {
-  return (
-    <>
-      <Helmet>
-        <meta charSet="utf-8" />
-        <html lang="fr" />
-        <title>Yves Molina Développeur</title>
-        <meta property="og:description" content="Portfolio de développement - Yves Molina"/>
-        <meta property="og:image" itemProp="image" content="./images/project.jpg"/>
-      </Helmet>
-
-      <App />
-    </>
-  )
+class Home extends React.Component {
+  render() {
+    return (
+      <div>
+        <Helmet>
+          <meta charSet="utf-8" />
+          <html lang="fr" />
+          <title>Yves Molina Développeur</title>
+          <description>test</description>
+          <meta name="description" content="My description2"/>
+          <meta
+            property="og:description"
+            content="Portfolio de développement - Yves Molina"
+          />
+          <meta
+            property="og:image"
+            itemProp="image"
+            content="./images/project.jpg"
+          />
+        </Helmet>
+        <App />
+      </div>
+    )
+  }
 }
+
+export default Home
